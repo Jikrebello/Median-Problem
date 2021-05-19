593d4a50a8f2e36e1c6dd5826c90d3572c372178
Jikrebello <33652252+Jikrebello@users.noreply.github.com>
2021-05-19 23:34:25 +0200

    Add files via upload

---
+++ Median Problem/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Median_Problem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtDisplay = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtDisplay
+            // 
+            this.TxtDisplay.Location = new System.Drawing.Point(12, 12);
+            this.TxtDisplay.Multiline = true;
+            this.TxtDisplay.Name = "TxtDisplay";
+            this.TxtDisplay.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtDisplay.Size = new System.Drawing.Size(224, 119);
+            this.TxtDisplay.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Median";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(251, 392);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TxtDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtDisplay;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Median Problem/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Median_Problem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        Random randGen = new Random();
+        void Populate_Data()
+        {
+            string path = Application.StartupPath + "/median.txt";
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    int randNum = randGen.Next(1, 100);
+                    writer.WriteLine(randNum);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = Application.StartupPath + "/median.txt";
+            List<int> data = new List<int>();
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    data.Add(Convert.ToInt32(reader.ReadLine()));
+                }
+            }
+
+            data.Sort();
+            TxtDisplay.Text += "The total of the values is: " + data.Count + Environment.NewLine;
+
+            foreach (var value in data) // Display the list of data once its sorted
+                TxtDisplay.Text += Convert.ToString(value) + Environment.NewLine;
+
+            double median;
+            if (data.Count % 2 == 0) // then its even
+            {
+                median = ((data.Count / 2) + (data.Count + 2) / 2) / 2;
+                TxtDisplay.Text += "Median for this data is: " + median + Environment.NewLine;
+            }
+            else if(data.Count % 2 != 0) // then its odd
+            {
+                median = (data.Count + 1) / 2;
+                TxtDisplay.Text += "Median for this data is: " + median + Environment.NewLine;
+            }
+        }
+
+    }
+}
